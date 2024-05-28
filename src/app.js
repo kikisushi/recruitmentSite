b148ba319ae70606972b968b9835088f95b5fec2
코딩배우는kikiSushi <165127332+kikisushi@users.noreply.github.com>
2024-05-28 17:04:50 +0900

    feat: 스키마 작성, 테이블 생성

--- src/app.js
+++ src/app.js
@@ -1,18 +1,16 @@
 // src/app.js
-
 import express from 'express';
 import cookieParser from 'cookie-parser';
-import prisma from './utils/prisma.util.js';
-import errorHandler from './middlewares/error-handler.middleware.js';
+import { errorHandler } from './middlewares/error-handler.middleware.js';
 
 const app = express();
 const PORT = 3018;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(prisma());
-app.use(errorHandler());
-app.use('/api', []);
+app.use(errorHandler);
+
+// app.use(errorHandler());
 
 app.listen(PORT, () => {
   console.log(PORT, '포트로 서버가 열렸어요!');
